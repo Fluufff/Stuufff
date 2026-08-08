c90735b833b3eea5353d7d05a97614277863f371
Glenn Dirkx <glenn.dirkx@belfius.be>
2026-08-08 22:54:01 +0200

    extra dummy pages

--- web/src/routes/routes.ts
+++ web/src/routes/routes.ts
@@ -5,8 +5,24 @@ export const routes = [
 		// URL path in web UI
 		path: resolve('/inventory'),
 		// Icon class, see src/lib/icons.css
-		icon: 'icon-[lucide--container] text-[32px]',
+		icon: 'icon-[mdi--archive-outline] text-[32px]',
 		// Menu name in web UI
 		name: 'Inventory'
+	},
+	{
+		// URL path in web UI
+		path: resolve('/reservations'),
+		// Icon class, see src/lib/icons.css
+		icon: 'icon-[mdi--truck-fast-outline] text-[32px]',
+		// Menu name in web UI
+		name: 'Reservations'
+	},
+	{
+		// URL path in web UI
+		path: resolve('/audit'),
+		// Icon class, see src/lib/icons.css
+		icon: 'icon-[mdi--archive-clock-outline] text-[32px]',
+		// Menu name in web UI
+		name: 'Audit logs'
 	}
 ];
